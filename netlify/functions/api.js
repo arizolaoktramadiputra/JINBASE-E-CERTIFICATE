45229568eb3688f9379c6c17b064c98c6b745194
MArizolaOkt <m.arizola@if.uai.ac.id>
2026-09-02 18:41:08 +0700

    Fix Netlify Blobs Lambda initialization

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,4 +1,12 @@
 const serverless = require('serverless-http');
+const { connectLambda } = require('@netlify/blobs');
 const app = require('../../server');
 
-exports.handler = serverless(app);
+const handler = serverless(app);
+
+exports.handler = async (event, context) => {
+    // Initialize Netlify Blobs for Lambda compatibility mode.
+    connectLambda(event);
+
+    return handler(event, context);
+};
